b207f1623157c4e02296bd783e891c59ccf02231
95-Samb <samuelbrown201195@hotmail.co.uk>
2019-06-11 13:43:53 +0100

    clean up frequency spec

--- day 1/find_repeat_frequency.rb
+++ day 1/find_repeat_frequency.rb
@@ -2,14 +2,15 @@ class FindRepeatFrequency
   def execute(input)
     first_input = input[0]
     second_input = input[1]
+    third_input = input[2]
 
     return 0 if input == [0]
 
     return nil if same_signs?(input)
-    return nil unless first_input.abs * 2 >= second_input.abs
-    return nil unless first_input.abs <= second_input.abs * 2
 
-    return 0 if converges_to_zero?(first_input, second_input)
+    return nil unless converges?(first_input, second_input,third_input)
+
+    return 0 if converges_to_zero?(first_input, second_input,)
 
     first_input
   end
@@ -22,7 +23,13 @@ class FindRepeatFrequency
     first_input.abs <= second_input.abs
   end
 
-  def converges_to_first_input?(first_input, second_input)
-    first_input.abs > second_input.abs
+  def converges?(first_input, second_input,third_input)
+    if third_input
+      return third_input.abs <= second_input.abs + first_input.abs
+    end
+    first_input.abs * 2 >= second_input.abs &&
+    first_input.abs <= second_input.abs * 2
   end
+
+
 end
--- day 1/spec/find_repeat_frequency_spec.rb
+++ day 1/spec/find_repeat_frequency_spec.rb
@@ -7,103 +7,146 @@ describe FindRepeatFrequency do
     end
   end
 
-  context 'for [1]' do
-    let(:input) { [1] }
-    it_behaves_like 'it returns', nil
+  context 'for single numbers' do
+    context 'for [1]' do
+      let(:input) { [1] }
+      it_behaves_like 'it returns', nil
+    end
+
+    context 'for [-1]' do
+      let(:input) { [-1] }
+      it_behaves_like 'it returns', nil
+    end
+
+    context 'for [0]' do
+      let(:input) { [0] }
+      it_behaves_like 'it returns', 0
+    end
   end
 
-  context 'for [-1]' do
-    let(:input) { [-1] }
-    it_behaves_like 'it returns', nil
+  context 'for two numbers' do
+    context 'for [-1, 1]' do
+      let(:input) { [-1, 1] }
+      it_behaves_like 'it returns', 0
+    end
+
+    context 'for [1, -1]' do
+      let(:input) { [1, -1] }
+      it_behaves_like 'it returns', 0
+    end
+
+    context 'for [2, -2]' do
+      let(:input) { [2, -2] }
+      it_behaves_like 'it returns', 0
+    end
+
+    context 'for [1, -2]' do
+      let(:input) { [1, -2] }
+      it_behaves_like 'it returns', 0
+    end
+
+    context 'for [-1, 2]' do
+      let(:input) { [-1, 2] }
+      it_behaves_like 'it returns', 0
+    end
+
+    context 'for [2, -1]' do
+      let(:input) { [2, -1] }
+      it_behaves_like 'it returns', 2
+    end
+
+    context 'for [-2, 1]' do
+      let(:input) { [-2, 1] }
+      it_behaves_like 'it returns', -2
+    end
+
+    context 'for [2, -3]' do
+      let(:input) { [2, -3] }
+      it_behaves_like 'it returns', 0
+    end
+
+    context 'for [7, -8]' do
+      let(:input) { [7, -8] }
+      it_behaves_like 'it returns', 0
+    end
+
+    context 'for [8, -7]' do
+      let(:input) { [8, -7] }
+      it_behaves_like 'it returns', 8
+    end
+
+    context 'for [1, -3]' do
+      let(:input) { [1, -3] }
+      it_behaves_like 'it returns', nil
+    end
+
+    context 'for [1, 2]' do
+      let(:input) { [1, 2] }
+      it_behaves_like 'it returns', nil
+    end
+
+    context 'for [15, -7]' do
+      let(:input) { [15, -7] }
+      it_behaves_like 'it returns', nil
+    end
   end
 
-  context 'for [-1, 1]' do
-    let(:input) { [-1, 1] }
-    it_behaves_like 'it returns', 0
+  context 'with three numbers' do
+    context 'for [-1, 1,-1]' do
+      let(:input) { [-1, 1, -1] }
+      it_behaves_like 'it returns', 0
+    end
+
+    context 'for [1, -3, 2]' do
+      let(:input) { [1, -3, 2] }
+      it_behaves_like 'it returns', 0
+    end
+
+    context 'for [1, 1, -10]' do
+      let(:input) { [1, 1, -10] }
+      it_behaves_like 'it returns', nil
+    end
   end
 
-  context 'for [1, -1]' do
-    let(:input) { [1, -1] }
-    it_behaves_like 'it returns', 0
-  end
+  xcontext 'with three numbers - ignored' do
+    context 'for [2, 3, -4]' do
+      let(:input) { [2, 3, -4] }
+      it_behaves_like 'it returns', 2
+    end
 
-  context 'for [2, -2]' do
-    let(:input) { [2, -2] }
-    it_behaves_like 'it returns', 0
-  end
+    context 'for [2, -4, 3]' do
+      let(:input) { [2, 3, -4] }
+      it_behaves_like 'it returns', 2
+    end
 
-  context 'for [1, -2]' do
-    let(:input) { [1, -2] }
-    it_behaves_like 'it returns', 0
-  end
+    context 'for [3, 2, -4]' do
+      let(:input) { [3, 2, -4] }
+      it_behaves_like 'it returns', 5
+    end
 
-  context 'for [-1, 2]' do
-    let(:input) { [-1, 2] }
-    it_behaves_like 'it returns', 0
-  end
+    context 'for [-4, 2, 3]' do
+      let(:input) { [-4, 2, 3] }
+      it_behaves_like 'it returns', -2
+    end
 
-  context 'for [2, -1]' do
-    let(:input) { [2, -1] }
-    it_behaves_like 'it returns', 2
-  end
+    context 'for [-4, 3, 2]' do
+      let(:input) { [-4, 3, 2] }
+      it_behaves_like 'it returns', 0
+    end
 
-  context 'for [-2, 1]' do
-    let(:input) { [-2, 1] }
-    it_behaves_like 'it returns', -2
-  end
+    context 'for [3, -4, 2]' do
+      let(:input) { [3, -4, 2] }
+      it_behaves_like 'it returns', 0
+    end
 
-  context 'for [2, -3]' do
-    let(:input) { [2, -3] }
-    it_behaves_like 'it returns', 0
-  end
+    context 'for [-10, 1, 1]' do
+      let(:input) { [-10, 1, 1] }
+      it_behaves_like 'it returns', nil
+    end
 
-  xcontext 'for [-1, 1,-1]' do
-    let(:input) { [-1, 1, -1] }
-    it_behaves_like 'it returns', 0
+    context 'for [3, 5, -6]' do
+      let(:input) { [3, 5, -6] }
+      it_behaves_like 'it returns', 8
+    end
   end
-
-  xcontext 'for [1, 2, -3]' do
-    let(:input) { [1, 2, -3] }
-    it_behaves_like 'it returns', 0
-  end
-
-  xcontext 'for [2, 3, -4]' do
-    let(:input) { [2, 3, -4] }
-    it_behaves_like 'it returns', 2
-  end
-
-  xcontext 'for [3, 5, -6]' do
-    let(:input) { [3, 5, -6] }
-    it_behaves_like 'it returns', 8
-  end
-
-  context 'for [7, -8]' do
-    let(:input) { [7, -8] }
-    it_behaves_like 'it returns', 0
-  end
-
-  context 'for [8, -7]' do
-    let(:input) { [8, -7] }
-    it_behaves_like 'it returns', 8
-  end
-
-  context 'for [1, -3]' do
-    let(:input) { [1, -3] }
-    it_behaves_like 'it returns', nil
-  end
-
-  context 'for [1, 2]' do
-    let(:input) { [1, 2] }
-    it_behaves_like 'it returns', nil
-  end
-
-  context 'for [0]' do
-    let(:input) { [0] }
-    it_behaves_like 'it returns', 0
-  end
-  context 'for [15, -7]' do
-    let(:input) { [15, -7] }
-    it_behaves_like 'it returns', nil
-  end
-
 end
