c5976b58007cf250d4ef19e4df1fff44cc96b6c1
95-Samb <samuelbrown201195@hotmail.co.uk>
2019-11-27 16:50:47 +0000

    alpha solution to problem

--- day 2/find_letters.rb
+++ day 2/find_letters.rb
@@ -1,8 +1,29 @@
 class FindSimilarBoxIdLetters
   def send(input)
     return nil if input.empty?
+    string_length = input[0].length
+    common_letters = {}
+    i = 0
+    while i + 1 < input.length
+      same_string = (same_letters(input[i],input[i + 1]))
+      common_letters[same_string.length] = same_string
+      i += 1
+    end
 
-    return (input[0].chars & input[1].chars).join if input[0][0] == input[1][0]
-    return (input[0].chars & input[1].chars).join if input[0][1] == input[1][1]
+
+    return (common_letters[string_length - 1] || nil)
+  end
+  def same_letters(first_input,second_input)
+    i = 0
+    letter_string = ""
+    while i < first_input.length
+      if first_input[i] == second_input[i]
+        letter_string += first_input[i]
+        i += 1
+      else
+        i += 1
+      end
+    end
+    letter_string
   end
 end
--- day 2/spec/find_letters_spec.rb
+++ day 2/spec/find_letters_spec.rb
@@ -1,6 +1,7 @@
 require_relative "../find_letters"
 
 describe FindSimilarBoxIdLetters do
+  x = FindSimilarBoxIdLetters.new
   shared_examples "it returns" do |parameter|
     it "#{parameter}" do
       expect(subject.send(input)).to eq(parameter)
@@ -10,28 +11,87 @@ describe FindSimilarBoxIdLetters do
     let(:input) {[]}
     it_behaves_like 'it returns', nil
   end
-  context "for [ab,ac]" do
-    let(:input) {["ab","ac"]}
+  context "for two inputs of length 2" do
+    context "for [ab,ac]" do
+      let(:input) {["ab","ac"]}
+      it_behaves_like 'it returns', "a"
+    end
+    context "for [ab,dc]" do
+      let(:input) {["ab","dc"]}
+      it_behaves_like 'it returns', nil
+    end
+    context "for [db,dc]" do
+      let(:input) {["db","dc"]}
+      it_behaves_like 'it returns', "d"
+    end
+    context "for [ab,cb]" do
+      let(:input) {["ab","cb"]}
+      it_behaves_like 'it returns', "b"
+    end
+  end
+  context "for two inputs of length 3" do
+    context "for [abc,def]" do
+      let(:input) {["abc","def"]}
+      it_behaves_like 'it returns', nil
+    end
+    context "for [abc,aef]" do
+      let(:input) {["abc","aef"]}
+      it_behaves_like 'it returns', nil
+    end
+    context "for [abc,abf]" do
+      let(:input) {["abc","abf"]}
+      it_behaves_like 'it returns', "ab"
+    end
+    context "for [aac,aaf]" do
+      let(:input) {["aac","aaf"]}
+      it_behaves_like 'it returns', "aa"
+    end
+    context "for [cba,abc]" do
+      let(:input) {["cba","abc"]}
+      it_behaves_like 'it returns', nil
+    end
+    context "for [aac,aca]" do
+      let(:input) {["aac","aca"]}
+      it_behaves_like 'it returns', nil
+    end
+    context "for [abcd,efgh]" do
+      let(:input) {["abcd","efgh"]}
+      it_behaves_like 'it returns', nil
+    end
+  end
+  context "for [abcd,efcd]" do
+    let(:input) {["abcd","efcd"]}
+    it_behaves_like 'it returns', nil
+  end
+  context "for [afcd,efcd]" do
+    let(:input) {["afcd","efcd"]}
+    it_behaves_like 'it returns', "fcd"
+  end
+  context "for [afcdaaaa,efcdbbbb]" do
+    let(:input) {["afcdaaaa","efcdbbbb"]}
+    it_behaves_like 'it returns', nil
+  end
+  context "for [ab,ac,de]" do
+    let(:input) {["ab","ac","de"]}
     it_behaves_like 'it returns', "a"
   end
-  context "for [ab,dc]" do
-    let(:input) {["ab","dc"]}
-    it_behaves_like 'it returns', nil
+  context "for [de,ac,ab]" do
+    let(:input) {["de","ac","ab"]}
+    it_behaves_like 'it returns', "a"
   end
-  context "for [db,dc]" do
-    let(:input) {["db","dc"]}
-    it_behaves_like 'it returns', "d"
+  context "for same_letters method" do
+    it "returns array of joint values" do
+      expect(x.same_letters("ab","ac")).to eq("a")
+    end
+    it "returns array of joint values" do
+      expect(x.same_letters("abcde","acdef")).to eq("a")
+    end
+    it "returns array of joint values" do
+      expect(x.same_letters("aaabb","aaacc")).to eq("aaa")
+    end
+    it "returns array of joint values" do
+      expect(x.same_letters("abcdefg","abefcdg")).to eq("abg")
+    end
   end
-  context "for [ab,cb]" do
-    let(:input) {["ab","cb"]}
-    it_behaves_like 'it returns', "b"
-  end
-  context "for [abc,def]" do
-    let(:input) {["abc","def"]}
-    it_behaves_like 'it returns', nil
-  end
-  xcontext "for [abc,aef]" do
-    let(:input) {["abc","aef"]}
-    it_behaves_like 'it returns', nil
-  end
+
 end
