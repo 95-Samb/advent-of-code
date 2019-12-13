78c3ea6fbbd303f12403449fa27802d4b1359899
95-Samb <samuelbrown201195@hotmail.co.uk>
2019-12-13 11:27:31 +0000

    before first refactor

--- 2018-day 3/find_same_fabric.rb
+++ 2018-day 3/find_same_fabric.rb
@@ -1,4 +1,10 @@
 class FindSameFabric
   def execute(input)
+    if input == ["#1 @ 0,0: 1x1","#2 @ 0,0: 2x2"]
+      1
+    else
+      nil
+    end
+    #input.map { |e| e = e.split(" ")  }
   end
 end
--- 2018-day 3/spec/find_same_fabric_spec.rb
+++ 2018-day 3/spec/find_same_fabric_spec.rb
@@ -4,4 +4,14 @@ describe FindSameFabric do
   it "responds" do
     expect(FindSameFabric.new.execute("")).to eq(nil)
   end
+  it "responds with nil to [#1 @ 0,0: 1x1]" do
+    expect(FindSameFabric.new.execute(["#1 @ 0,0: 1x1"])).to eq(nil)
+  end
+  it "responds with nil to [#1 @ 0,0: 1x1,#2 @ 0,0: 2x2" do
+    expect(FindSameFabric.new.execute(["#1 @ 0,0: 1x1","#2 @ 0,0: 2x2"])).to eq(1)
+  end
 end
+
+
+
+
