0d10b2a10b93d0b5f156422e2c55e0fe60a1045b
95-Samb <samuelbrown201195@hotmail.co.uk>
2020-01-06 11:32:06 +0000

    beginning to expand beyond single opcode

--- 2019-day 2/intcode.rb
+++ 2019-day 2/intcode.rb
@@ -1,9 +1,18 @@
 class Intcode
   def execute(input)
-    if input[0] == 1
-      input[input[3]] = input[input[1]] + input[input[2]]
+    i = 0
+    if input[i] == 1
+      input[input[i + 3]] = input[input[i + 1]] + input[input[i + 2]]
     else
-      input[input[3]] = input[input[1]] * input[input[2]]
+      input[input[i + 3]] = input[input[i + 1]] * input[input[i + 2]]
+    end
+    if input[4] != 99
+      i += 4
+      if input[i] == 1
+        input[input[i + 3]] = input[input[i + 1]] + input[input[i + 2]]
+      else
+        input[input[i + 3]] = input[input[i + 1]] * input[input[i + 2]]
+      end
     end
     input
   end
--- 2019-day 2/spec/intcode_spec.rb
+++ 2019-day 2/spec/intcode_spec.rb
@@ -1,26 +1,33 @@
 require_relative "../intcode.rb"
 
 describe Intcode do
-  it "returns 2,0,0,0,99 for 1,0,0,0,99" do
-    expect(Intcode.new.execute([1,0,0,0,99])).to eq([2,0,0,0,99])
+  context "for a single 4 opcode before 99" do
+    it "returns 2,0,0,0,99 for 1,0,0,0,99" do
+      expect(Intcode.new.execute([1,0,0,0,99])).to eq([2,0,0,0,99])
+    end
+    it "returns 2,1,0,0,99 for 1,1,0,0,99" do
+      expect(Intcode.new.execute([1,1,0,0,99])).to eq([2,1,0,0,99])
+    end
+    it "returns 1,2,0,1,99 for 1,1,0,1,99" do
+      expect(Intcode.new.execute([1,1,0,1,99])).to eq([1,2,0,1,99])
+    end
+    it "returns 2,1,0,0,99 for 2,1,0,0,99" do
+      expect(Intcode.new.execute([2,1,0,0,99])).to eq([2,1,0,0,99])
+    end
+    it "returns 1,2,1,2,99 for 1,2,0,2,99" do
+      expect(Intcode.new.execute([1,2,0,2,99])).to eq([1,2,1,2,99])
+    end
+    it "returns 2,2,6,2,99 for 2,2,3,2,99" do
+      expect(Intcode.new.execute([2,2,3,2,99])).to eq([2,2,6,2,99])
+    end
+    it "returns 1,0,1,2,99,0 for 1,0,5,2,99,0" do
+      expect(Intcode.new.execute([1,0,5,2,99,0])).to eq([1,0,1,2,99,0])
+    end
   end
-  it "returns 2,1,0,0,99 for 1,1,0,0,99" do
-    expect(Intcode.new.execute([1,1,0,0,99])).to eq([2,1,0,0,99])
-  end
-  it "returns 1,2,0,1,99 for 1,1,0,1,99" do
-    expect(Intcode.new.execute([1,1,0,1,99])).to eq([1,2,0,1,99])
-  end
-  it "returns 2,1,0,0,99 for 2,1,0,0,99" do
-    expect(Intcode.new.execute([2,1,0,0,99])).to eq([2,1,0,0,99])
-  end
-  it "returns 1,2,1,2,99 for 1,2,0,2,99" do
-    expect(Intcode.new.execute([1,2,0,2,99])).to eq([1,2,1,2,99])
-  end
-  it "returns 2,2,6,2,99 for 2,2,3,2,99" do
-    expect(Intcode.new.execute([2,2,3,2,99])).to eq([2,2,6,2,99])
-  end
-  it "returns 1,0,1,2,99,0 for 1,0,5,2,99,0" do
-    expect(Intcode.new.execute([1,0,5,2,99,0])).to eq([1,0,1,2,99,0])
+  context "for a double opcode before 99" do
+    it "returns 2,4,0,0,1,0,0,1,99 for 1,0,0,0,1,0,0,1,99" do
+      expect(Intcode.new.execute([1,0,0,0,1,0,0,1,99])).to eq([2,4,0,0,1,0,0,1,99])
+    end
   end
 
 end
