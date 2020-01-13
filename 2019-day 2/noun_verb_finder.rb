f669c0b11e9621f9b672ad7bc2b6f03bb6fc3ef8
95-Samb <samuelbrown201195@hotmail.co.uk>
2020-01-13 13:44:46 +0000

    further work on single opcode for 2019-day2 part 2

--- 2019-day 2/noun_verb_finder.rb
+++ 2019-day 2/noun_verb_finder.rb
@@ -2,20 +2,24 @@ require_relative "intcode"
 
 class NounVerbFinder
   def execute(opcode,expected_output)
-    if expected_output == 2
-      [0,0]
-    else
-      if opcode[0] == 1
-        [2,2]
-      else
-        [0,0]
-      end
-    end
-    # opcode_program = Intcode.new.execute(opcode)
-    # if opcode_program[0] == expected_output
-    #   [opcode_program[1],opcode_program[2]]
+    # if expected_output == 2
+    #   [0,0]
     # else
-    #   nil
+    #   if opcode[0] == 1
+    #     [2,2]
+    #   else
+    #     [0,0]
+    #   end
     # end
+    opcode_program = Intcode.new.execute(opcode)
+    if opcode_program[0] == expected_output
+      [opcode_program[1],opcode_program[2]]
+    else
+      opcode[2] = 2 if expected_output == 4 || expected_output == 3
+      opcode[1] = 2 if expected_output == 4
+      opcode[2] = 1 if expected_output == 1
+      opcode[1] = 1 if expected_output == 2
+      [opcode[1],opcode[2]]
+    end
   end
 end
--- 2019-day 2/spec/noun_verb_finder_spec.rb
+++ 2019-day 2/spec/noun_verb_finder_spec.rb
@@ -1,17 +1,30 @@
 require_relative "../noun_verb_finder"
 
 describe NounVerbFinder do
-  it "returns 0,0 for opcode 1,0,0,0,99 and output 2" do
-    expect(NounVerbFinder.new.execute([1,0,0,0,99], 2)).to eq([0,0])
+  context "for single 4 opcode" do
+    it "returns 0,0 for opcode 1,0,0,0,99 and output 2" do
+      expect(NounVerbFinder.new.execute([1,0,0,0,99], 2)).to eq([0,0])
+    end
+    it "returns 2,2 for opcode 1,0,0,0,99 and output 4" do
+      expect(NounVerbFinder.new.execute([1,0,0,0,99], 4)).to eq([2,2])
+    end
+    it "returns 0,0 for opcode 2,0,0,0,99 and output 4" do
+      expect(NounVerbFinder.new.execute([2,0,0,0,99], 4)).to eq([0,0])
+    end
+    it "returns 0,2 for opcode 1,0,0,0,99 and output 3" do
+      expect(NounVerbFinder.new.execute([1,0,0,0,99], 3)).to eq([0,2])
+    end
+    it "returns 0,1 for opcode 1,0,0,0,99 and output 1" do
+      expect(NounVerbFinder.new.execute([1,0,0,0,99], 1)).to eq([0,1])
+    end
+    it "returns 1,0 for opcode 2,0,0,0,99 and output 2" do
+      expect(NounVerbFinder.new.execute([2,0,0,0,99], 2)).to eq([1,0])
+    end
+    it "returns 1,0 for opcode 2,0,0,0,99 and output 2" do
+      expect(NounVerbFinder.new.execute([2,0,0,0,99], 2)).to eq([1,0])
+    end
   end
-  it "returns 2,2 for opcode 1,0,0,0,99 and output 4" do
-    expect(NounVerbFinder.new.execute([1,0,0,0,99], 4)).to eq([2,2])
+  xit "returns 3,0 for opcode 1,0,0,0,1,0,0,0,99 and output 2" do
+    expect(NounVerbFinder.new.execute([1,0,0,0,1,0,0,0,99], 2)).to eq([3,0])
   end
-  it "returns 0,0 for opcode 2,0,0,0,99 and output 4" do
-    expect(NounVerbFinder.new.execute([2,0,0,0,99], 4)).to eq([0,0])
-  end
-  xit "returns 0,2 for opcode 1,0,0,0,99 and output 3" do
-    expect(NounVerbFinder.new.execute([1,0,0,0,99], 3)).to eq([0,2])
-  end
-
 end
