a72f4a3326638116dc620922f3aaae908751252b
95-Samb <samuelbrown201195@hotmail.co.uk>
2020-02-24 16:57:43 +0000

    initial compelete duplicate adjacent numbers method and initial work on increasing digits method for aoe 2019 day4

--- 2019-day 4/password_finder.rb
+++ 2019-day 4/password_finder.rb
@@ -3,7 +3,25 @@ class PasswordFinder
   def execute(input)
   end
 
-  def duplicate_number(input)
-    true
+  def duplicate_adjacent_number(input)
+    input_array = input.to_s.split("")
+    i = 0
+    until i == input_array.length
+      if input_array[i] == input_array[i + 1]
+        return true
+      else
+        i += 1
+      end
+    end
+    false
+  end
+
+  def increasing_digits?(input)
+    input_array = input.to_s.split("")
+    if input_array[0] < input_array[1]
+      true
+    else
+      false
+    end
   end
 end
--- 2019-day 4/spec/password_finder_spec.rb
+++ 2019-day 4/spec/password_finder_spec.rb
@@ -1,9 +1,38 @@
 require_relative "../password_finder"
 
 describe PasswordFinder do
-  context "for duplicate_number method" do
+  context "for duplicate_adjacent_number method" do
     it "returns true for 11" do
-      expect(PasswordFinder.new.duplicate_number(11)).to eq(true)
+      expect(PasswordFinder.new.duplicate_adjacent_number(11)).to eq(true)
+    end
+    it "returns false for 12" do
+      expect(PasswordFinder.new.duplicate_adjacent_number(12)).to eq(false)
+    end
+    it "returns true for 22" do
+      expect(PasswordFinder.new.duplicate_adjacent_number(22)).to eq(true)
+    end
+    it "returns false for 212" do
+      expect(PasswordFinder.new.duplicate_adjacent_number(212)).to eq(false)
+    end
+    it "returns true for 221" do
+      expect(PasswordFinder.new.duplicate_adjacent_number(221)).to eq(true)
+    end
+    it "returns true for 122345" do
+      expect(PasswordFinder.new.duplicate_adjacent_number(122345)).to eq(true)
+    end
+    it "returns false for 123245" do
+      expect(PasswordFinder.new.duplicate_adjacent_number(123245)).to eq(false)
+    end
+    it "returns true for 122225" do
+      expect(PasswordFinder.new.duplicate_adjacent_number(122225)).to eq(true)
+    end
+  end
+  context "for the increasing_digits? method" do
+    it "returns true for 12" do
+      expect(PasswordFinder.new.increasing_digits?(12)).to eq(true)
+    end
+    it "returns true for 21" do
+      expect(PasswordFinder.new.increasing_digits?(21)).to eq(false)
     end
   end
 end
