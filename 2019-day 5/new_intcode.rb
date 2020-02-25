e36002ec91865e59839d6fde3ac45a4d0dcc9032
95-Samb <samuelbrown201195@hotmail.co.uk>
2020-02-25 15:06:57 +0000

    initial day 5 commit by copying code over from day 2

---
+++ 2019-day 5/new_intcode.rb
@@ -0,0 +1,14 @@
+class NewIntcode
+  def execute(input)
+    i = 0
+    while input[i] != 99
+      input[input[i + 3]] = if input[i] == 1
+                              input[input[i + 1]] + input[input[i + 2]]
+                            else
+                              input[input[i + 1]] * input[input[i + 2]]
+                            end
+      i += 4
+    end
+    input
+  end
+  end
---
+++ 2019-day 5/spec/new_intcode_spec.rb
@@ -0,0 +1,49 @@
+require_relative "../new_intcode"
+
+describe NewIntcode do
+  context 'for a single 4 opcode before 99' do
+    it 'returns 2,0,0,0,99 for 1,0,0,0,99' do
+      expect(NewIntcode.new.execute([1, 0, 0, 0, 99])).to eq([2, 0, 0, 0, 99])
+    end
+    it 'returns 2,1,0,0,99 for 1,1,0,0,99' do
+      expect(NewIntcode.new.execute([1, 1, 0, 0, 99])).to eq([2, 1, 0, 0, 99])
+    end
+    it 'returns 1,2,0,1,99 for 1,1,0,1,99' do
+      expect(NewIntcode.new.execute([1, 1, 0, 1, 99])).to eq([1, 2, 0, 1, 99])
+    end
+    it 'returns 2,1,0,0,99 for 2,1,0,0,99' do
+      expect(NewIntcode.new.execute([2, 1, 0, 0, 99])).to eq([2, 1, 0, 0, 99])
+    end
+    it 'returns 1,2,1,2,99 for 1,2,0,2,99' do
+      expect(NewIntcode.new.execute([1, 2, 0, 2, 99])).to eq([1, 2, 1, 2, 99])
+    end
+    it 'returns 2,2,6,2,99 for 2,2,3,2,99' do
+      expect(NewIntcode.new.execute([2, 2, 3, 2, 99])).to eq([2, 2, 6, 2, 99])
+    end
+    it 'returns 1,0,1,2,99,0 for 1,0,5,2,99,0' do
+      expect(NewIntcode.new.execute([1, 0, 5, 2, 99, 0])).to eq([1, 0, 1, 2, 99, 0])
+    end
+    it 'returns 1,6,2,2,99,0,2 for 1,6,5,2,99,0,2' do
+      expect(NewIntcode.new.execute([1, 6, 5, 2, 99, 0, 2])).to eq([1, 6, 2, 2, 99, 0, 2])
+    end
+  end
+  context 'for a double opcode before 99' do
+    it 'returns 2,4,0,0,1,0,0,1,99 for 1,0,0,0,1,0,0,1,99' do
+      expect(NewIntcode.new.execute([1, 0, 0, 0, 1, 0, 0, 1, 99])).to eq([2, 4, 0, 0, 1, 0, 0, 1, 99])
+    end
+    it 'returns 2,4,0,0,2,0,0,1,99 for 1,0,0,0,2,0,0,1,99' do
+      expect(NewIntcode.new.execute([1, 0, 0, 0, 2, 0, 0, 1, 99])).to eq([2, 4, 0, 0, 2, 0, 0, 1, 99])
+    end
+    it 'returns 1,1,1,0,2,0,0,1,99 for 1,0,1,0,2,0,0,1,99' do
+      expect(NewIntcode.new.execute([1, 0, 1, 0, 2, 0, 0, 1, 99])).to eq([1, 1, 1, 0, 2, 0, 0, 1, 99])
+    end
+    it 'returns 1,1,4,1,2,0,0,1,99 for 1,0,4,1,2,0,0,1,99' do
+      expect(NewIntcode.new.execute([1, 0, 4, 1, 2, 0, 0, 1, 99])).to eq([1, 1, 4, 1, 2, 0, 0, 1, 99])
+    end
+  end
+  context 'for a triple opcode before 99' do
+    it 'returns 4,8,0,0,1,0,0,0,1,0,0,1,99 for 1,0,0,0,1,0,0,0,1,0,0,1,99' do
+      expect(NewIntcode.new.execute([1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 99])).to eq([4, 8, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 99])
+    end
+  end
+end
