f65eca7aef94249cb01ce62ad6bdc494e003b004
95-Samb <samuelbrown201195@hotmail.co.uk>
2020-03-04 17:34:31 +0000

    initial work on logic for aoe 2019 day 6

--- 2019-day 6/orbits.rb
+++ 2019-day 6/orbits.rb
@@ -1,6 +1,24 @@
 
 class Orbits
   def execute(input)
-    input.length
+    orbits = {}
+    answer = input.length
+    answer += 1 if input[1] && input[1][0] == "B"
+    answer
+  end
+  def orbit_logic(input)
+    answer = [input[0][0],input[0][2]]
+    answer.push(input[1][2]) if input[1] && input[1][0] != input[0][0]
+    answer[1] += input[1][2] if input[1][0] == input[0][0]
+    answer
+  end
+  def all_orbiters(input)
+    i = 0
+    answer = []
+    while i < input.length
+      answer.push(input[i][0]).push(input[i][2])
+      i += 1
+    end
+    answer.uniq
   end
 end
--- 2019-day 6/spec/orbits_spec.rb
+++ 2019-day 6/spec/orbits_spec.rb
@@ -1,10 +1,46 @@
 require_relative "../orbits"
 
 describe Orbits do
-  it "returns 1 for A)B" do
-    expect(Orbits.new.execute(["A)B"])).to eq(1)
+  context "for the execute method" do
+    it "returns 1 for A)B" do
+      expect(Orbits.new.execute(["A)B"])).to eq(1)
+    end
+    it "returns 2 for A)B,A)C" do
+      expect(Orbits.new.execute(["A)B","A)C"])).to eq(2)
+    end
+    it "returns 3 for A)B,B)C" do
+      expect(Orbits.new.execute(["A)B","B)C"])).to eq(3)
+    end
+    xit "returns 3 for B)C,A)B" do
+      expect(Orbits.new.execute(["B)C","A)B"])).to eq(3)
+    end
   end
-  it "returns 2 for A)B,A)C" do
-    expect(Orbits.new.execute(["A)B","A)C"])).to eq(2)
+  xcontext "for the orbit_logic method" do
+    it "returns [A,B] for A)B" do
+      expect(Orbits.new.orbit_logic(["A)B"])).to eq(["A","B"])
+    end
+    it "returns [A,B,C] for A)B,B)C" do
+      expect(Orbits.new.orbit_logic(["A)B","A)C"])).to eq(["A","B","C"])
+    end
+    it "returns [A,B,D] for A)B,B)D" do
+      expect(Orbits.new.orbit_logic(["A)B","A)D"])).to eq(["A","B","D"])
+    end
+    it "returns [A,BC] for A)B,A)C" do
+      expect(Orbits.new.orbit_logic(["A)B","A)C"])).to eq(["A","BC"])
+    end
+  end
+  context "for the all_orbiters method " do
+    it "returns [A,B] for A)B" do
+      expect(Orbits.new.all_orbiters(["A)B"])).to eq(["A","B"])
+    end
+    it "returns [A,B,C] for A)B,B)C" do
+      expect(Orbits.new.all_orbiters(["A)B","B)C"])).to eq(["A","B","C"])
+    end
+    it "returns [A,B,C] for A)B,A)C" do
+      expect(Orbits.new.all_orbiters(["A)B","A)C"])).to eq(["A","B","C"])
+    end
+    it "returns [A,B,C,D] for A)B,A)C,B)D" do
+      expect(Orbits.new.all_orbiters(["A)B","A)C","B)D"])).to eq(["A","B","C","D"])
+    end
   end
 end
