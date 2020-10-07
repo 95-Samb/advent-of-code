5f12ecb796e147653abdcf59e5c12254de7732a2
95-Samb <samuelbrown201195@hotmail.co.uk>
2020-10-07 17:04:30 +0100

    added new layer adding method for 2019 day 8 part 2

--- 2019-day-8/image_renderer.rb
+++ 2019-day-8/image_renderer.rb
@@ -2,4 +2,20 @@ class ImageRenderer
   def decode(layers)
     layers[0]
   end
+  def combine_layers(top_layer,bottom_layer)
+    new_layer = top_layer.dup.map(&:chars)
+    new_bottom_layer = bottom_layer.dup.map(&:chars)
+    i = 0
+    j = 0
+    while i < new_layer.length
+      new_layer[i][j] = new_bottom_layer[i][j] if new_layer[i][j] == "2"
+      j += 1
+      if j == new_layer[i].length
+        j = 0
+        i += 1
+      end
+    end
+    new_layer.map(&:join)
+
+  end
 end
--- 2019-day-8/spec/image_renderer_spec.rb
+++ 2019-day-8/spec/image_renderer_spec.rb
@@ -15,4 +15,27 @@ describe ImageRenderer do
       expect(ImageRenderer.new.decode([["2","1"],["1","0"]])).to eq(["1","1"])
     end
   end
+  context "for layer combiner" do
+    it "returns 0 for 0 and 0" do
+      expect(ImageRenderer.new.combine_layers(["0"],["0"])).to eq(["0"])
+    end
+    it "returns 1 for 1 and 0" do
+      expect(ImageRenderer.new.combine_layers(["1"],["0"])).to eq(["1"])
+    end
+    it "returns 1,0 for 1,1 and 0,1" do
+      expect(ImageRenderer.new.combine_layers(["1","1"],["0","1"])).to eq(["1","1"])
+    end
+    it "returns 1,1 for 2,1 and 1,0" do
+      expect(ImageRenderer.new.combine_layers(["2","1"],["1","0"])).to eq(["1","1"])
+    end
+    it "returns 1,1 for 2,1 and 1,0" do
+      expect(ImageRenderer.new.combine_layers(["2","1"],["1","0"])).to eq(["1","1"])
+    end
+    it "returns 10,11 for 20,11 and 11,01" do
+      expect(ImageRenderer.new.combine_layers(["20","11"],["11","01"])).to eq(["10","11"])
+    end
+    it "returns 00,11 for 20,11 and 01,01" do
+      expect(ImageRenderer.new.combine_layers(["20","11"],["01","01"])).to eq(["00","11"])
+    end
+  end
 end
