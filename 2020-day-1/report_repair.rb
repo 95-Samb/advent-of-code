98c888086cbfcab5830eaf374e18fa9897f7f1d8
95-Samb <samuelbrown201195@hotmail.co.uk>
2020-12-17 18:25:59 +0000

    further work, using a product method(inject)

--- 2020-day-1/report_repair.rb
+++ 2020-day-1/report_repair.rb
@@ -1,10 +1,14 @@
 class ReportRepair
 
   def execute(input)
-    if input.sum == 2020
-      input.inject(&:*)
-    else
-      nil
+
+    return nil if input.kind_of?(Array) == false || input.length < 2
+
+
+    if input[0..1].sum == 2020
+      input[0..1].inject(&:*)
+    elsif input[1..2].sum == 2020
+      input[1..2].inject(&:*)
     end
   end
 
--- 2020-day-1/spec/report_repair_spec.rb
+++ 2020-day-1/spec/report_repair_spec.rb
@@ -13,4 +13,16 @@ describe ReportRepair do
     it "returns 2019 for array with 1 and 2019" do
       expect(ReportRepair.new.execute([1,2019])).to eq(2019)
     end
+
+    it "returns 2019 for array with 1,2019,2" do
+      expect(ReportRepair.new.execute([1,2019,2])).to eq(2019)
+    end
+
+    it "returns 2019 for array with 2,2019,1" do
+      expect(ReportRepair.new.execute([2,2019,1])).to eq(2019)
+    end
+
+    xit "returns 2019 for array with 2019,2,1" do
+      expect(ReportRepair.new.execute([2019,2,1])).to eq(2019)
+    end
 end
