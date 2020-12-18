36869b79a5932e13874fea21b8fb77e35178bbc4
95-Samb <samuelbrown201195@hotmail.co.uk>
2020-12-18 10:49:12 +0000

    pulled finding pair logic into its own method with some initial tests

--- 2020-day-1/report_repair.rb
+++ 2020-day-1/report_repair.rb
@@ -4,11 +4,15 @@ class ReportRepair
 
     return nil if input.kind_of?(Array) == false || input.length < 2
 
-    pairs = input.combination(2).to_a
-
-    correct_pair = pairs.keep_if {|pair| pair.sum == 2020}.flatten
+    correct_pair = find_correct_pair(input)
 
     correct_pair.inject(:*)
+  end
+
+  def find_correct_pair(input)
+    pairs = input.combination(2).to_a
+
+    correct_pair = pairs.keep_if {|pair| pair.sum == 2020}.flatten.sort
   end
 
 end
--- 2020-day-1/spec/report_repair_spec.rb
+++ 2020-day-1/spec/report_repair_spec.rb
@@ -1,6 +1,7 @@
 require_relative "../report_repair"
 
 describe ReportRepair do
+  context "for execute method" do
 
     it "returns nil for empty string" do
       expect(ReportRepair.new.execute("")).to eq(nil)
@@ -25,4 +26,22 @@ describe ReportRepair do
     it "returns 2019 for array with 2019,2,1" do
       expect(ReportRepair.new.execute([2019,2,1])).to eq(2019)
     end
+
+    it "returns 2019 for array with 2019,2,1,3,4,5,6,7" do
+      expect(ReportRepair.new.execute([2019,2,1,3,4,5,6,7])).to eq(2019)
+    end
+
+    it "returns nil for array with 1,2,3,4,5,6,7,8,9" do
+      expect(ReportRepair.new.execute([1,2,3,4,5,6,7,8,9])).to eq(nil)
+    end
+  end
+
+  context "for find correct pair method" do
+    it "returns 1,2019 for array with 2019,2,1,3,4,5,6,7" do
+      expect(ReportRepair.new.find_correct_pair([2019,2,1,3,4,5,6,7])).to eq([1,2019])
+    end
+    it "returns 20,2000 for array with 2000,20,1,3,4,5,6,7" do
+      expect(ReportRepair.new.find_correct_pair([2000,20,1,3,4,5,6,7])).to eq([20,2000])
+    end
+  end
 end
