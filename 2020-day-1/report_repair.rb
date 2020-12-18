dd158dee1bd21b163ae9b2194572b068220d64aa
95-Samb <samuelbrown201195@hotmail.co.uk>
2020-12-18 11:12:06 +0000

    modified 2020 day 1 methods to consider how many numbers we require to add up to 2020

--- 2020-day-1/report_repair.rb
+++ 2020-day-1/report_repair.rb
@@ -1,16 +1,16 @@
 class ReportRepair
 
-  def execute(input)
+  def execute(input,numbers_amount)
 
     return nil if input.kind_of?(Array) == false || input.length < 2
 
-    correct_pair = find_correct_pair(input)
+    correct_pair = find_correct_pair(input,numbers_amount)
 
     correct_pair.inject(:*)
   end
 
-  def find_correct_pair(input)
-    pairs = input.combination(2).to_a
+  def find_correct_pair(input,numbers_amount)
+    pairs = input.combination(numbers_amount).to_a
 
     correct_pair = pairs.keep_if {|pair| pair.sum == 2020}.flatten.sort
   end
--- 2020-day-1/spec/report_repair_spec.rb
+++ 2020-day-1/spec/report_repair_spec.rb
@@ -4,44 +4,44 @@ describe ReportRepair do
   context "for execute method" do
 
     it "returns nil for empty string" do
-      expect(ReportRepair.new.execute("")).to eq(nil)
+      expect(ReportRepair.new.execute("",2)).to eq(nil)
     end
 
     it "returns nil for array with no valid values" do
-      expect(ReportRepair.new.execute([])).to eq(nil)
+      expect(ReportRepair.new.execute([],2)).to eq(nil)
     end
 
     it "returns 2019 for array with 1 and 2019" do
-      expect(ReportRepair.new.execute([1,2019])).to eq(2019)
+      expect(ReportRepair.new.execute([1,2019],2)).to eq(2019)
     end
 
     it "returns 2019 for array with 1,2019,2" do
-      expect(ReportRepair.new.execute([1,2019,2])).to eq(2019)
+      expect(ReportRepair.new.execute([1,2019,2],2)).to eq(2019)
     end
 
     it "returns 2019 for array with 2,2019,1" do
-      expect(ReportRepair.new.execute([2,2019,1])).to eq(2019)
+      expect(ReportRepair.new.execute([2,2019,1],2)).to eq(2019)
     end
 
     it "returns 2019 for array with 2019,2,1" do
-      expect(ReportRepair.new.execute([2019,2,1])).to eq(2019)
+      expect(ReportRepair.new.execute([2019,2,1],2)).to eq(2019)
     end
 
     it "returns 2019 for array with 2019,2,1,3,4,5,6,7" do
-      expect(ReportRepair.new.execute([2019,2,1,3,4,5,6,7])).to eq(2019)
+      expect(ReportRepair.new.execute([2019,2,1,3,4,5,6,7],2)).to eq(2019)
     end
 
     it "returns nil for array with 1,2,3,4,5,6,7,8,9" do
-      expect(ReportRepair.new.execute([1,2,3,4,5,6,7,8,9])).to eq(nil)
+      expect(ReportRepair.new.execute([1,2,3,4,5,6,7,8,9],2)).to eq(nil)
     end
   end
 
   context "for find correct pair method" do
     it "returns 1,2019 for array with 2019,2,1,3,4,5,6,7" do
-      expect(ReportRepair.new.find_correct_pair([2019,2,1,3,4,5,6,7])).to eq([1,2019])
+      expect(ReportRepair.new.find_correct_pair([2019,2,1,3,4,5,6,7],2)).to eq([1,2019])
     end
     it "returns 20,2000 for array with 2000,20,1,3,4,5,6,7" do
-      expect(ReportRepair.new.find_correct_pair([2000,20,1,3,4,5,6,7])).to eq([20,2000])
+      expect(ReportRepair.new.find_correct_pair([2000,20,1,3,4,5,6,7],2)).to eq([20,2000])
     end
   end
 end
