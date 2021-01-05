0a0865880c30d2716dbfde21fa609aaf411243b4
95-Samb <samuelbrown201195@hotmail.co.uk>
2021-01-05 11:30:20 +0000

    added logic to password formatter to return integer values for min and max

--- 2020-day-2/password_validater.rb
+++ 2020-day-2/password_validater.rb
@@ -1,19 +1,19 @@
 class PasswordValidater
   def execute(input)
-    formatted_passwords = input[0].split(" ")
-    return 1 if formatted_passwords[2].split("").any?(formatted_passwords[1][0])
+    formatted_passwords = password_formatter(input[0])
+    return 1 if password_validater(formatted_passwords)
   end
 
   def password_formatter(input)
     formatted_input = input.split(" ")
-    formatted_input[0] = formatted_input[0].split("-")
+    formatted_input[0] = formatted_input[0].split("-").map(&:to_i)
     formatted_input[1].chop!
     formatted_input.flatten
   end
 
   def password_validater(input)
     if input[3].include? input[2]
-      true if input[3].count(input[2]) >= input[0].to_i && input[3].count(input[2]) <= input[1].to_i
+      true if input[3].count(input[2]) >= input[0] && input[3].count(input[2]) <= input[1]
     else
     end
   end
--- 2020-day-2/spec/password_validater_spec.rb
+++ 2020-day-2/spec/password_validater_spec.rb
@@ -16,42 +16,42 @@ describe PasswordValidater do
   it "returns 1 for [1-1 b: abcdefghjk]" do
     expect(PasswordValidater.new.execute(["1-1 b: abcdefghjk"])).to eq(1)
   end
-  xit "returns 1 for [1-1 b: bb]" do
+  it "returns nil for [1-1 b: bb]" do
     expect(PasswordValidater.new.execute(["1-1 b: bb"])).to eq(nil)
   end
 
   context "for password formatter method" do
     it "returns [1,1,a,a] for 1-1 a: a" do
-      expect(PasswordValidater.new.password_formatter("1-1 a: a")).to eq(["1","1","a","a"])
+      expect(PasswordValidater.new.password_formatter("1-1 a: a")).to eq([1,1,"a","a"])
     end
     it "returns [1,2,a,abcdef] for 1-2 a: abcdef" do
-      expect(PasswordValidater.new.password_formatter("1-2 a: abcdef")).to eq(["1","2","a","abcdef"])
+      expect(PasswordValidater.new.password_formatter("1-2 a: abcdef")).to eq([1,2,"a","abcdef"])
     end
   end
   context "for password validater method" do
     it "returns true for [1,1,a,a] " do
-      expect(PasswordValidater.new.password_validater(["1","1","a","a"])).to eq(true)
+      expect(PasswordValidater.new.password_validater([1,1,"a","a"])).to eq(true)
     end
     it "returns nil for [1,1,a,b] " do
-      expect(PasswordValidater.new.password_validater(["1","1","a","b"])).to eq(nil)
+      expect(PasswordValidater.new.password_validater([1,1,"a","b"])).to eq(nil)
     end
     it "returns true for [1,1,a,ba] " do
-      expect(PasswordValidater.new.password_validater(["1","1","a","ba"])).to eq(true)
+      expect(PasswordValidater.new.password_validater([1,1,"a","ba"])).to eq(true)
     end
     it "returns true for [1,7,a,ba] " do
-      expect(PasswordValidater.new.password_validater(["1","7","a","ba"])).to eq(true)
+      expect(PasswordValidater.new.password_validater([1,7,"a","ba"])).to eq(true)
     end
     it "returns nil for [2,2,a,ba] " do
-      expect(PasswordValidater.new.password_validater(["2","2","a","ba"])).to eq(nil)
+      expect(PasswordValidater.new.password_validater([2,2,"a","ba"])).to eq(nil)
     end
     it "returns true for [2,2,a,baa] " do
-      expect(PasswordValidater.new.password_validater(["2","2","a","baa"])).to eq(true)
+      expect(PasswordValidater.new.password_validater([2,2,"a","baa"])).to eq(true)
     end
     it "returns true for [2,5,a,baaaa] " do
-      expect(PasswordValidater.new.password_validater(["2","5","a","baaaa"])).to eq(true)
+      expect(PasswordValidater.new.password_validater([2,5,"a","baaaa"])).to eq(true)
     end
     it "returns false for [2,5,a,baaaaaa] " do
-      expect(PasswordValidater.new.password_validater(["2","5","a","baaaaaa"])).to eq(nil)
+      expect(PasswordValidater.new.password_validater([2,5,"a","baaaaaa"])).to eq(nil)
     end
   end
 end
