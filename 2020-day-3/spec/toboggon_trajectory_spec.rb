6fec5128e552015df8701d0d2c36d1576b1f216b
95-Samb <samuelbrown201195@hotmail.co.uk>
2021-01-15 17:52:13 +0000

    initial consideration for input requiring repeating

--- 2020-day-3/spec/toboggon_trajectory_spec.rb
+++ 2020-day-3/spec/toboggon_trajectory_spec.rb
@@ -10,9 +10,17 @@ describe TobogganTrajectory do
   it "returns 0 for ...,...,..." do
     expect(TobogganTrajectory.new.execute(["....","....","...."])).to eq(0)
   end
-  it "returns 0 for ...,...,..." do
+  it "returns 1 for ...,...,..." do
     expect(TobogganTrajectory.new.execute(
       ["...." * 2 ,"...." * 2,"......#"])).to eq(1)
+  end
+  it "returns 0 for ...,...,..." do
+    expect(TobogganTrajectory.new.execute(
+      ["...." ,"...." ])).to eq(0)
+  end
+  it "returns 0 for ...,...,..." do
+    expect(TobogganTrajectory.new.execute(
+      ["..." ,"...","#.." ])).to eq(1)
   end
 
 end
--- 2020-day-3/toboggan_trajectory.rb
+++ 2020-day-3/toboggan_trajectory.rb
@@ -3,7 +3,7 @@ class TobogganTrajectory
     i = 1
     output = 0
     while i < input.length
-      if input[i][i * 3] == "#"
+      if input[i][i * 3 % input[0].length] == "#"
         output += 1
       end
       i += 1
