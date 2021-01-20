641bf300c1a7b1f6c024d92291dd651706f41fc3
95-Samb <samuelbrown201195@hotmail.co.uk>
2021-01-20 17:03:05 +0000

    edited wrong worded tests and added more complicated tests

--- 2020-day-3/spec/toboggon_trajectory_spec.rb
+++ 2020-day-3/spec/toboggon_trajectory_spec.rb
@@ -7,20 +7,38 @@ describe TobogganTrajectory do
   it "returns 1 for ....,...#" do
     expect(TobogganTrajectory.new.execute(["....","...#"])).to eq(1)
   end
-  it "returns 0 for ...,...,..." do
+  it "returns 0 for ....,....,...." do
     expect(TobogganTrajectory.new.execute(["....","....","...."])).to eq(0)
   end
-  it "returns 1 for ...,...,..." do
+  it "returns 1 for ........,........,......#" do
     expect(TobogganTrajectory.new.execute(
       ["...." * 2 ,"...." * 2,"......#"])).to eq(1)
   end
-  it "returns 0 for ...,...,..." do
+  it "returns 0 for ....,...." do
     expect(TobogganTrajectory.new.execute(
       ["...." ,"...." ])).to eq(0)
   end
-  it "returns 0 for ...,...,..." do
+  it "returns 1 for ...,...,#.." do
     expect(TobogganTrajectory.new.execute(
       ["..." ,"...","#.." ])).to eq(1)
+  end
+  it "returns 2 for ...,#..,#.." do
+    expect(TobogganTrajectory.new.execute(
+      ["..." ,"#..","#.." ])).to eq(2)
+  end
+  it "returns 7 for given input" do
+    expect(TobogganTrajectory.new.execute(
+      ["..##.......",
+      "#...#...#..",
+      ".#....#..#.",
+      "..#.#...#.#",
+      ".#...##..#.",
+      "..#.##.....",
+      ".#.#.#....#",
+      ".#........#",
+      "#.##...#...",
+      "#...##....#",
+      ".#..#...#.#"])).to eq(7)
   end
 
 end
