71b5cac270197d6e28169dc6afe8f0a31754de02
95-Samb <samuelbrown201195@hotmail.co.uk>
2021-02-08 10:54:41 +0000

    initial work on row identify method highlighting halving impasse

--- 2020-day-5/spec/boarding_pass_spec.rb
+++ 2020-day-5/spec/boarding_pass_spec.rb
@@ -5,5 +5,14 @@ describe BoardingPass do
     it "returns 0 for FFFFFFFLLL" do
       expect(BoardingPass.new("FFFFFFFLLL").row_identify).to eq(0)
     end
+    it "returns 64 for BFFFFFFLLL" do
+      expect(BoardingPass.new("BFFFFFFLLL").row_identify).to eq(64)
+    end
+    it "returns 96 for BBFFFFFLLL" do
+      expect(BoardingPass.new("BBFFFFFLLL").row_identify).to eq(96)
+    end
+    it "returns 112 for BBBFFFFLLL" do
+      expect(BoardingPass.new("BBBFFFFLLL").row_identify).to eq(112)
+    end
   end
 end
