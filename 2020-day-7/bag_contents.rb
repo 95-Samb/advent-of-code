e147b5de2f08c25a0aed4272d6cdde5397111baa
95-Samb <samuelbrown201195@hotmail.co.uk>
2021-02-15 11:22:45 +0000

    initial commit for 2020 day 7 aoc

---
+++ 2020-day-7/bag_contents.rb
@@ -0,0 +1,5 @@
+class BagContents
+  def execute(input)
+    {"shiny gold bag" => 1}
+  end
+end
---
+++ 2020-day-7/spec/bag_contents_spec.rb
@@ -0,0 +1,9 @@
+require_relative "../bag_contents"
+
+describe BagContents do
+  it "returns 1 shiny gold bag for bright white bags contain 1 shiny gold bag." do
+    expect(BagContents.new.execute("bright white bags contain 1 shiny gold bag.")).
+    to eq({"shiny gold bag" => 1})
+  end
+
+end
