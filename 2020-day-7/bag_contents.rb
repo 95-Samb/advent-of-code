b15212476efb75f50824978f6e6a50244ec62755
95-Samb <samuelbrown201195@hotmail.co.uk>
2021-02-15 12:07:50 +0000

    added logic for handling multiple contents BagContents class

--- 2020-day-7/bag_contents.rb
+++ 2020-day-7/bag_contents.rb
@@ -1,6 +1,12 @@
 class BagContents
   def execute(input)
-    contents = input.split("contain ")[1]
-    {contents[2..-2] => contents[0].to_i}
+    contents = input.chop.split("contain ")[1].split(", ")
+    contents_hash = {}
+    contents.each { |e|
+      if e[-1] == "s"
+        contents_hash[e[2..-2]] = e[0].to_i
+      else contents_hash[e[2..-1]] = e[0].to_i
+      end}
+    contents_hash
   end
 end
--- 2020-day-7/spec/bag_contents_spec.rb
+++ 2020-day-7/spec/bag_contents_spec.rb
@@ -9,5 +9,15 @@ describe BagContents do
     expect(BagContents.new.execute("bright white bags contain 1 shiny green bag.")).
     to eq({"shiny green bag" => 1})
   end
+  subject = "light red bags contain 1 bright white bag, 1 muted yellow bag."
+  it "returns 1 bright white bag, 1 muted yellow bag for #{subject}" do
+    expect(BagContents.new.execute(subject)).
+    to eq({"bright white bag" => 1,"muted yellow bag" => 1})
+  end
+  subject2 = "light red bags contain 1 bright white bag, 2 muted yellow bags."
+  it "returns 1 bright white bag, 2 muted yellow bags for #{subject2}" do
+    expect(BagContents.new.execute(subject2)).
+    to eq({"bright white bag" => 1,"muted yellow bag" => 2})
+  end
 
 end
