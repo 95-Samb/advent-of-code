b5e3f43a42521f9dfa6c9a5a64bb1199f67d8b10
95-Samb <samuelbrown201195@hotmail.co.uk>
2021-02-16 14:59:01 +0000

    initial work on bags indirectly containing desired bag

--- 2020-day-7/bag_processing.rb
+++ 2020-day-7/bag_processing.rb
@@ -4,11 +4,17 @@ class BagProcessing
     @bags = bags
   end
 
+  def totally_contains(bag)
+    directly_contained = directly_contains(bag)
+    indirectly_contained = directly_contains(directly_contained[0])
+    directly_contained + indirectly_contained
+  end
+
   def directly_contains(bag)
     contains = []
 
     @bags.each do  |e|
-      if e[:contents]["shiny gold bag"]
+      if e[:contents][bag]
         contains.push(e[:name])
       end
     end
--- 2020-day-7/spec/bag_processing_spec.rb
+++ 2020-day-7/spec/bag_processing_spec.rb
@@ -1,15 +1,32 @@
 require_relative "../bag_processing"
 
 describe BagProcessing do
-  subject = [{name:"bright white bag",contents:{"shiny gold bag" => 1}}]
-  subject2 = [{name:"bright white bag",contents:{"shiny gold bag" => 1}},
-    {name:"matte white bag",contents:{"shiny gold bag" => 1}}]
-  it "returns bright white bag for subject and shiny gold bag" do
-    expect(BagProcessing.new(subject).directly_contains("shiny gold bag")).
-    to eq(["bright white bag"])
+  context "for directly contains method" do
+    subject = [{name:"bright white bag",contents:{"shiny gold bag" => 1}}]
+    subject2 = [{name:"bright white bag",contents:{"shiny gold bag" => 1}},
+      {name:"matte white bag",contents:{"shiny gold bag" => 1}}]
+    it "returns bright white bag for #{subject} and shiny gold bag" do
+      expect(BagProcessing.new(subject).directly_contains("shiny gold bag")).
+      to eq(["bright white bag"])
+    end
+    it "returns bright white bag and matte white bag for #{subject2} and shiny gold bag" do
+      expect(BagProcessing.new(subject2).directly_contains("shiny gold bag")).
+      to eq(["bright white bag","matte white bag"])
+    end
   end
-  it "returns bright white bag and matte white bag for subject and shiny gold bag" do
-    expect(BagProcessing.new(subject2).directly_contains("shiny gold bag")).
-    to eq(["bright white bag","matte white bag"])
+  context "for totally contains method" do
+    subject = [{name:"bright white bag",contents:{"shiny gold bag" => 1}},
+    {name:"matte white bag",contents:{"bright white bag" => 1}}]
+    subject2 = [{name:"bright white bag",contents:{"shiny gold bag" => 1}},
+    {name:"bright yellow bag",contents:{"bright white bag" => 1}}]
+    it "returns bright white bag and matte white bag for #{subject} and shiny gold bag" do
+      expect(BagProcessing.new(subject).totally_contains("shiny gold bag")).
+      to eq(["bright white bag","matte white bag"])
+    end
+    it "returns bright white bag and bright yellow bag for #{subject2} and shiny gold bag" do
+      expect(BagProcessing.new(subject2).totally_contains("shiny gold bag")).
+      to eq(["bright white bag","bright yellow bag"])
+    end
+
   end
 end
