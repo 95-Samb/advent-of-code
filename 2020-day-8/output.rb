33ba5c50578e4a687f4e038154c14df0f07c9de3
95-Samb <samuelbrown201195@hotmail.co.uk>
2021-03-31 12:39:25 +0100

    updated output script to provide answer for day 8 part 2

--- 2020-day-8/output.rb
+++ 2020-day-8/output.rb
@@ -5,6 +5,12 @@ input = File.read("input.txt")
 
 boot_code = BootCodeFormatter.new.execute(input)
 
-accumulator = BootCodeComputer.new(boot_code).execute
+accumulator = BootCodeComputer.new(boot_code,"").execute
+
+array_of_accumulators = BootCodeComputer.new(boot_code,"last instruction").infinite_fix
+
+array_of_accumulators.delete(nil)
 
 puts accumulator
+
+puts array_of_accumulators
