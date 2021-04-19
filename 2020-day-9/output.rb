c4c516cae72a153f81d1067bf5cbbc81c3fd8c59
95-Samb <samuelbrown201195@hotmail.co.uk>
2021-04-19 11:05:39 +0100

    added second answer for day 9 2020 to output script

--- 2020-day-9/output.rb
+++ 2020-day-9/output.rb
@@ -7,4 +7,8 @@ numbers = InputConverter.new.execute(input)
 
 answers = AdditionSystem.new(numbers).no_pair_sum(25)
 
+consecutive_numbers = AdditionSystem.new(numbers).continuous_sum(answers[0])
+
 puts answers[0]
+
+puts consecutive_numbers.sort[0] + consecutive_numbers.sort[-1]
