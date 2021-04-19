05822ed348fc5220270e5ecb4d3eb62ceb3f1bdf
95-Samb <samuelbrown201195@hotmail.co.uk>
2021-04-19 15:59:12 +0100

    added sort method to initialize method to keep logic consistent

--- 2020-day-10/adapter_configurator.rb
+++ 2020-day-10/adapter_configurator.rb
@@ -1,12 +1,12 @@
 class AdapterConfigurator
 
   def initialize(adapters)
-    @adapters = adapters
+    @adapters = adapters.sort
   end
 
   def jolt_differences
 
-    return nil if @adapters.empty?
+    return {one: 0, three: 0} if @adapters.empty?
     return {one: 0, three: 0} if @adapters[0] == 2
     return {one: 1,three: 1} if @adapters[1]
     {one: 1}
--- 2020-day-10/spec/adapter_configurator_spec.rb
+++ 2020-day-10/spec/adapter_configurator_spec.rb
@@ -5,7 +5,7 @@ describe AdapterConfigurator do
   context "#jolt_differences" do
 
     it "returns nil for no adapters" do
-      expect(AdapterConfigurator.new([]).jolt_differences).to eq nil
+      expect(AdapterConfigurator.new([]).jolt_differences).to eq ({one: 0, three: 0})
     end
 
     it "returns one difference of 1 for [1]" do
@@ -16,8 +16,16 @@ describe AdapterConfigurator do
       expect(AdapterConfigurator.new([1,4]).jolt_differences).to eq ({one: 1,three: 1})
     end
 
+    it "returns one difference of 1 and one of 3 for [4,1]" do
+      expect(AdapterConfigurator.new([4,1]).jolt_differences).to eq ({one: 1,three: 1})
+    end
+
     it "returns no relevant number of differences for [2,4]" do
       expect(AdapterConfigurator.new([2,4]).jolt_differences).to eq ({one: 0,three: 0})
+    end
+
+    it "returns no relevant number of differences for [4,2]" do
+      expect(AdapterConfigurator.new([4,2]).jolt_differences).to eq ({one: 0,three: 0})
     end
 
   end
