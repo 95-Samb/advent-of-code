ccd78eab924787d5db875da8beaeee822e92721b
95-Samb <samuelbrown201195@hotmail.co.uk>
2021-05-12 16:33:26 +0100

    initial complete working adjacent seats method

--- 2020-day-11/seating_system.rb
+++ 2020-day-11/seating_system.rb
@@ -20,6 +20,19 @@ class SeatingSystem
     if coords[0] - 1 >= 0
       adjacents.push(@seats[coords[0] - 1][coords[1]])
     end
+    if coords[1] - 1 >= 0 && coords[0] + 1 < @seats.length
+      adjacents.push(@seats[coords[0] + 1][coords[1] - 1])
+    end
+    if coords[1] - 1 >= 0 && coords[0] - 1 >= 0
+      adjacents.push(@seats[coords[0] - 1][coords[1] - 1])
+    end
+    if coords[1] + 1 < @seats[0].length && coords[0] + 1 < @seats.length
+      adjacents.push(@seats[coords[0] + 1][coords[1] + 1])
+    end
+    if coords[1] + 1 < @seats[0].length && coords[0] - 1 >= 0
+      adjacents.push(@seats[coords[0] - 1][coords[1] + 1])
+    end
+
 
     adjacents.flatten
 
--- 2020-day-11/spec/seating_system_spec.rb
+++ 2020-day-11/spec/seating_system_spec.rb
@@ -30,9 +30,20 @@ describe SeatingSystem do
         ).identify_adjacents([0,0])).to eq(["L"])
     end
 
-    it "returns [L] for 0,1 and L,L" do
+    it "returns [L] for 1,0 and L,L" do
       expect(SeatingSystem.new([["L"],["L"]]
         ).identify_adjacents([1,0])).to eq(["L"])
+    end
+
+    it "returns [L,#] for 1,0 and L,L,#" do
+      expect(SeatingSystem.new([["L"],["L"],["#"]]
+        ).identify_adjacents([1,0])).to match_array(["L","#"])
+    end
+
+    it "returns L..L.#.. for 1,1 and L..,LL.,#.." do
+      expect(SeatingSystem.new([["L",".","."],["L","L","."],["#",".","."]]
+        ).identify_adjacents([1,1])).
+      to match_array(["L",".",".","L",".","#",".","."])
     end
 
   end
