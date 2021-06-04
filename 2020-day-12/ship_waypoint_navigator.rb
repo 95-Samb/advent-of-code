aaab435dcf07602f397e9afd671e910b4fc86076
95-Samb <samuelbrown201195@hotmail.co.uk>
2021-06-04 15:56:04 +0100

    transpose over old #take_action with minor modifacations for part 2

--- 2020-day-12/ship_waypoint_navigator.rb
+++ 2020-day-12/ship_waypoint_navigator.rb
@@ -7,8 +7,13 @@ class ShipWaypointNavigator
 
   def take_action(action)
 
-    @waypoint[0] += 1
+    waypoint_direction_hash = {"N" => [0,action[1..-1].to_i],"E" => [action[1..-1].to_i,0] ,
+      "S" => [0,-(action[1..-1].to_i)],"W" => [-(action[1..-1].to_i),0]}
 
+    if waypoint_direction_hash[action[0]]
+      @waypoint[0] += waypoint_direction_hash[action[0]][0]
+      @waypoint[1] += waypoint_direction_hash[action[0]][1]
+    end
   end
 
 end
--- 2020-day-12/spec/ship_waypoint_navigator_spec.rb
+++ 2020-day-12/spec/ship_waypoint_navigator_spec.rb
@@ -13,6 +13,26 @@ describe ShipWaypointNavigator do
       expect(start.instance_variable_get(:@waypoint)).to eq([11,1])
 
     end
+
+    it "returns 0,0,E and 10,2 for N1" do
+
+      start = ShipWaypointNavigator.new
+      start.take_action("N1")
+
+      expect(start.instance_variable_get(:@position)).to eq([0,0,"E"])
+      expect(start.instance_variable_get(:@waypoint)).to eq([10,2])
+
+    end
+
+    it "returns 0,0,E and -1,1 for W11" do
+
+      start = ShipWaypointNavigator.new
+      start.take_action("W11")
+
+      expect(start.instance_variable_get(:@position)).to eq([0,0,"E"])
+      expect(start.instance_variable_get(:@waypoint)).to eq([-1,1])
+
+    end
   end
 
 end
