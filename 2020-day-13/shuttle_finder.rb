68552d1609bfe5edbc4e4f297e64c3a14f981b8c
95-Samb <samuelbrown201195@hotmail.co.uk>
2021-06-08 11:01:47 +0100

    initial work on #earliest_bus for day 13 2020

--- 2020-day-13/shuttle_finder.rb
+++ 2020-day-13/shuttle_finder.rb
@@ -1,3 +1,13 @@
 class ShuttleFinder
 
+  def initialize(time,buses)
+    @time = time
+    @buses = buses
+  end
+
+  def earliest_bus
+    return nil if @buses.empty?
+    [@buses[0],@buses[0]]
+  end
+
 end
--- 2020-day-13/spec/shuttle_finder_spec.rb
+++ 2020-day-13/spec/shuttle_finder_spec.rb
@@ -2,4 +2,24 @@ require_relative "../shuttle_finder"
 
 describe ShuttleFinder do
 
+  context "#earliest_bus" do
+
+    it "returns 1,1 for 1,[1]" do
+      expect(ShuttleFinder.new(1,[1]).earliest_bus).to eq ([1,1])
+    end
+
+    it "returns nil for 0,[]" do
+      expect(ShuttleFinder.new(0,[]).earliest_bus).to eq (nil)
+    end
+
+    it "returns 1,1 for 0,[1]" do
+      expect(ShuttleFinder.new(0,[1]).earliest_bus).to eq ([1,1])
+    end
+
+    it "returns 2,2 for 2,[2,3]" do
+      expect(ShuttleFinder.new(2,[2,3]).earliest_bus).to eq ([2,2])
+    end
+
+  end
+
 end
