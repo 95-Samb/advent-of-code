55083183f3e2fb2838fa1f1f49075c466b424e55
95-Samb <samuelbrown201195@hotmail.co.uk>
2021-06-08 13:36:39 +0100

    added loop to handle non multiples of given time for #earliest_bus

--- 2020-day-13/shuttle_finder.rb
+++ 2020-day-13/shuttle_finder.rb
@@ -7,8 +7,13 @@ class ShuttleFinder
 
   def earliest_bus
     return nil if @buses.empty?
-    earliest = @buses.select{ |bus| @time % bus == 0}.first
-    [earliest,earliest]
+    return "any bus numpty" if @time == 0
+
+    loop do
+      earliest = @buses.select{ |bus| @time % bus == 0}.first
+      return [earliest,@time] if earliest
+      @time += 1
+    end
   end
 
 end
--- 2020-day-13/spec/shuttle_finder_spec.rb
+++ 2020-day-13/spec/shuttle_finder_spec.rb
@@ -13,7 +13,7 @@ describe ShuttleFinder do
     end
 
     it "returns 1,1 for 0,[1]" do
-      expect(ShuttleFinder.new(0,[1]).earliest_bus).to eq ([1,1])
+      expect(ShuttleFinder.new(0,[1]).earliest_bus).to eq ("any bus numpty")
     end
 
     it "returns 2,2 for 2,[2,3]" do
@@ -26,6 +26,14 @@ describe ShuttleFinder do
 
     it "returns 5,5 for 5,[5,2]" do
       expect(ShuttleFinder.new(5,[5,2]).earliest_bus).to eq ([5,5])
+    end
+
+    it "returns 7,7 for 6,[7,8]" do
+      expect(ShuttleFinder.new(6,[7,8]).earliest_bus).to eq ([7,7])
+    end
+
+    it "returns 3,9 for 8,[3,5,7]" do
+      expect(ShuttleFinder.new(8,[3,5,7]).earliest_bus).to eq ([3,9])
     end
 
   end
