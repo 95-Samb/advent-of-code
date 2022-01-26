06b9744e7b96a14ab0d782c4c321f146d425eb2f
Sam <samuelbrown201195@hotmail.co.uk>
2022-01-26 12:57:02 +0000

    work on execute loop for aoc 2021 day 1 part 1

--- 2021-day-1/increase_calculator.py
+++ 2021-day-1/increase_calculator.py
@@ -3,6 +3,12 @@ class IncreaseCalculator:
 	def __init__(self):
 		self.increase_count = 0
 
+	def execute(self,number_list):
+		i = 0
+		while i + 1 < len(number_list):
+			self.increase_counter(number_list[i],number_list[i + 1])
+			i += 1
+
 	def increase_counter(self,first,second):
 		if second > first: self.increase_count += 1 
 			
--- 2021-day-1/tests/test_increase_calculator.py
+++ 2021-day-1/tests/test_increase_calculator.py
@@ -18,3 +18,21 @@ class TestIncreaseCounter(unittest.TestCase):
 		calculator.increase_counter(2,1)
 
 		self.assertEqual(calculator.increase_count,0)
+
+class TestExecute(unittest.TestCase):
+
+	def test_first(self):
+
+		calculator = IncreaseCalculator()
+
+		calculator.execute([1,2])
+
+		self.assertEqual(calculator.increase_count,1)
+
+	def test_second(self):
+
+		calculator = IncreaseCalculator()
+
+		calculator.execute([1,2,3,2,1])
+
+		self.assertEqual(calculator.increase_count,2)
